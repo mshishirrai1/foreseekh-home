a74252c1ee04237dc18602a6be8a7e677d58d708
“prajwal” <prajwalraim@gmail.com>
2024-06-25 22:02:16 +0530

    image-path for static

--- src/app/modules/shared/follow-us/follow-us.component.ts
+++ src/app/modules/shared/follow-us/follow-us.component.ts
@@ -10,23 +10,23 @@ export class FollowUsComponent {
     title:"Follow us on:",
     iconList:[
       {
-        path:"/assets/images/footer/facebook.svg",
+        path:"/foreseekh-home/assets/images/footer/facebook.svg",
         url:"https://www.facebook.com/profile.php?id=100082986573710"
       },
       {
-        path:"/assets/images/footer/instagram.svg",
+        path:"/foreseekh-home/assets/images/footer/instagram.svg",
         url:"https://www.facebook.com/profile.php?id=100082986573710"
       },
       {
-        path:"/assets/images/footer/linkedin.svg",
+        path:"/foreseekh-home/assets/images/footer/linkedin.svg",
         url:"https://www.linkedin.com/in/foreseekh-foreseekh-211927243"
       },
       {
-        path:"/assets/images/footer/twitter.svg",
+        path:"/foreseekh-home/assets/images/footer/twitter.svg",
         url:"https://www.linkedin.com/in/foreseekh-foreseekh-211927243"
       },
       {
-        path:"/assets/images/footer/youtube.svg",
+        path:"/foreseekh-home/assets/images/footer/youtube.svg",
         url:"https://www.linkedin.com/in/foreseekh-foreseekh-211927243"
       }
     ]
